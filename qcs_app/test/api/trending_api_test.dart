04d292eabc75b1a58d852a9aab8d7a5dad90ab97
marius <marius@macbook-air-de-marius.home>
2023-01-16 15:06:18 +0100

    test(api): all api test

--- qcs_app/test/api/trending_api_test.dart
+++ qcs_app/test/api/trending_api_test.dart
@@ -8,28 +8,68 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'dart:convert';
+
 import 'package:http/http.dart';
 import 'package:openapi/api.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../models/crypto_model.dart';
+import '../provider/trending_coins_provider.dart';
+import '../repository/trending_coins_repository.dart';
 
 /// tests for TrendingApi
 void main() {
   // final instance = TrendingApi();
 
   group('tests for TrendingApi', () {
-    test('test searchTrendingGet', () async {
-      Future<String?> GetTrending() async {
+    test('Get fullJson Trending without logic', () async {
+      Future<Response?> getTrending() async {
         final instance = TrendingApi(ApiClient());
-
         final result = await instance.searchTrendingGetWithHttpInfo();
-        print(result.body.toString());
-        return result.body;
+        return result;
       }
 
-      print(await GetTrending());
-      //String tRes = '{"gecko_says":"(V3) To the Moon!"}';
-      //expect(() => ping(), returnsNormally);
-      //expect(await ping(), tRes);
+      final json = await getTrending();
+      expect(() => getTrending(), returnsNormally);
+      expect(json!.body.isNotEmpty, true);
+    });
+
+    test('Get trending coins', () async {
+      Future<String> getJson() async {
+        final jsonString = await TrendingProvider().getTrending();
+        return jsonString!.body;
+      }
+
+      final jsonData = await getJson();
+      final Map<String, dynamic>? data =
+          jsonDecode(jsonData) as Map<String, dynamic>?;
+
+      data!.remove("exchanges");
+
+      final dynamic newItemList =
+          data['coins'].map((dynamic e) => e["item"]).toList();
+
+      print(newItemList.runtimeType);
+
+      for (var i = 0; i < 7; i++) {
+        print("-----------------------------------------------------");
+        print(newItemList[i]);
+        // expect(newItemList[i], true);
+      }
+    });
+
+    test('All logic test: model, provider, repo', () async {
+      final result = await TrendingRepository.getTrendingRepo();
+      expect(result!.isNotEmpty, true);
+      expect(result.length, 7);
+
+      for (var i = 0; i < result.length; i++) {
+        expect(result[i].name.isNotEmpty, true);
+        expect(result[i].symbol.isNotEmpty, true);
+        expect(result[i].thumb.isNotEmpty, true);
+      }
+      
     });
   });
 }
